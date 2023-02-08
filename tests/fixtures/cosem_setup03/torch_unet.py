02b7b179f8b600cd7164dd0124c9ed6503b13761
pattonw <pattonw@hhmi.org>
2023-02-08 15:38:42 -0500

    add tests with info on how to they work

---
+++ tests/fixtures/cosem_setup03/torch_unet.py
@@ -0,0 +1,44 @@
+import torch
+from funlib.learn.torch.models.unet import UNet
+from funlib.geometry import Coordinate
+
+# voxel size parameters
+voxel_size_labels = Coordinate((2,) * 3)
+voxel_size = Coordinate((4,) * 3)
+voxel_size_input = Coordinate((4,) * 3)
+
+# network parameters copied from unet_template.py
+padding = "valid"
+constant_upsample = True
+trans_equivariant = True
+feature_widths_down = [12, 12 * 6, 12 * 6 ** 2, 12 * 6 ** 3]
+feature_widths_up = [12 * 6, 12 * 6, 12 * 6 ** 2, 12 * 6 ** 3]
+downsampling_factors = [(2,) * 3, (3,) * 3, (3,) * 3]
+kernel_sizes_down = [
+    [(3,) * 3, (3,) * 3],
+    [(3,) * 3, (3,) * 3],
+    [(3,) * 3, (3,) * 3],
+    [(3,) * 3, (3,) * 3],
+]
+kernel_sizes_up = [[(3,) * 3, (3,) * 3], [(3,) * 3, (3,) * 3], [(3,) * 3, (3,) * 3]]
+
+# additional network parameters for upsampling network
+upsample_factor = tuple(voxel_size_input / voxel_size)
+final_kernel_size = [(3,) * 3, (3,) * 3]
+final_feature_width = 12 * 6
+
+# build the torch unet equivalent
+torch_model = UNet(
+    in_channels=1,
+    num_fmaps=12,
+    num_fmaps_out=72,
+    fmap_inc_factor=6,
+    downsample_factors=[(2, 2, 2), (3, 3, 3), (3, 3, 3)],
+    upsample_channel_contraction=[False, True, True],
+    constant_upsample=True,
+    activation_on_upsample=True,
+)
+torch_model = torch.nn.Sequential(
+    torch_model, torch.nn.Conv3d(72, 14, (1, 1, 1), (1, 1, 1))
+)
+
---
+++ tests/test_cosem_setup04.py
@@ -0,0 +1,59 @@
+import torch
+import numpy as np
+
+from funlib.learn.torch.models.unet import UNet, ConvPass, Upsample
+
+from cnnectome.conversion import transfer_weights, compare
+
+from .datadir import datadir
+
+
+def test_cosem_setup04(datadir):
+    # generate some random input data
+    input_shape = (216, 216, 216)
+    np.random.seed()
+    in_data = np.random.randn(*input_shape)
+
+    # path to the tensorflow checkpoint
+    tf_path = f"{(datadir / 'unet_train_checkpoint_999500').resolve()}"
+    print(tf_path)
+
+    # build the torch unet equivalent
+    upsample_factor = (2, 2, 2)
+    final_kernel_size = [(3,) * 3, (3,) * 3]
+    final_feature_width = 12 * 6
+    downsampling_factors = [(2,) * 3, (3,) * 3, (3,) * 3]
+
+    torch_model = UNet(
+        in_channels=1,
+        num_fmaps=12,
+        num_fmaps_out=72,
+        fmap_inc_factor=6,
+        downsample_factors=downsampling_factors,
+        upsample_channel_contraction=[False, True, True],
+        constant_upsample=True,
+        activation_on_upsample=True,
+    )
+    torch_model = torch.nn.Sequential(
+        torch_model,
+        Upsample(
+            upsample_factor,
+            mode="nearest",
+            in_channels=final_feature_width,
+            out_channels=final_feature_width,
+            activation="ReLU",
+        ),
+        ConvPass(
+            final_feature_width,
+            final_feature_width,
+            final_kernel_size,
+            activation="ReLU",
+        ),
+        torch.nn.Conv3d(72, 14, (1, 1, 1), (1, 1, 1)),
+    )
+
+    # import weights from tensorflow model to torch model
+    transfer_weights(tf_path, torch_model)
+    # compare models. Will fail assertion if the two models
+    # don't produce almost identical results
+    compare(tf_path, torch_model, in_data)
